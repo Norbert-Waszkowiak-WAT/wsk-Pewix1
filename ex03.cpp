25cdb42d5bef8267d8db38fe7943b174c7327848
Pewix1 <166026566+Pewix1@users.noreply.github.com>
2025-11-06 07:18:42 +0000

    rozwiązanie 3

---
+++ ex03.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void min_max(int*arr,int size,int*min,int*max){
+    *max=*arr;
+    *min=*arr;
+    for(int* p=arr+1;p<arr+size;p++){
+        if(*p>*max){
+            *max=*p;
+        }
+        if(*p<*min){
+            *min=*p;
+        }
+    }
+}
+
+
+int main(){
+
+    int a[5]={5,10,3,20,16};
+    int y,x;
+    min_max(a,5,&y,&x);
+    cout<<"najmniejsza: "<<y<<endl;
+    cout<<"najwieksza:"<<x<<endl;
+
+
+    int b[5]={1,50,2,158,200};
+    min_max(b,5,&y,&x);
+    cout<<"najmniejsza: "<<y<<endl;
+    cout<<"najwieksza:"<<x<<endl;
+    return 0;
+
+}
